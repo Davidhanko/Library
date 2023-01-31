6ceb6778ab6f513c49abb03e7109dc5329d66530
Davihan11 <davihan11@gmail.com>
2023-01-31 21:51:56 +0100

    Done for now

--- script.js
+++ script.js
@@ -11,40 +11,45 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
-  createHTMLFromLibrary();
 }
 
-function createHTMLFromLibrary() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i].title;
-    let author = myLibrary[i].author;
-    let pages = myLibrary[i].pages;
-    let read = myLibrary[i].read;
-    createHTML(book, author, pages, read);
-  }
-}
+// function createHTMLFromLibrary() {
+//   for (let i = 0; i < myLibrary.length; i++) {
+//     let book = myLibrary[i].title;
+//     let author = myLibrary[i].author;
+//     let pages = myLibrary[i].pages;
+//     let read = myLibrary[i].read;
+//     createHTML(book, author, pages, read);
+//   }
+// }
 
-function createHTML(book, author, pages, read) {
+function createHTML(book, author, pages) {
   let bookshelf = document.getElementById("bookshelf");
   let bookDiv = document.createElement("div");
   bookDiv.classList.add("book");
   bookshelf.appendChild(bookDiv);
+
   let bookTitle = document.createElement("h2");
   bookTitle.classList.add("bookTitle");
-  bookTitle.innerHTML = book;
+  bookTitle.innerHTML = "Title: " + book;
   bookDiv.appendChild(bookTitle);
+
   let bookAuthor = document.createElement("p");
   bookAuthor.classList.add("bookAuthor");
-  bookAuthor.innerHTML = author;
+  bookAuthor.innerHTML = "By: " + author;
   bookDiv.appendChild(bookAuthor);
+
   let bookPages = document.createElement("p");
   bookPages.classList.add("bookPages");
-  bookPages.innerHTML = pages;
+  bookPages.innerHTML = "Has: " + pages + " pages";
   bookDiv.appendChild(bookPages);
-  let bookRead = document.createElement("p");
+
+  let bookRead = document.createElement("button");
   bookRead.classList.add("bookRead");
-  bookRead.innerHTML = read;
+  changeBookRead(bookRead);
+  bookRead.addEventListener("click", () => changeBookRead(bookRead));
   bookDiv.appendChild(bookRead);
+
   let bookDelete = document.createElement("button");
   bookDelete.classList.add("bookDelete");
   bookDelete.textContent = "Delete";
@@ -55,9 +60,18 @@ function createHTML(book, author, pages, read) {
   });
 }
 
+function changeBookRead(bookRead) {
+  if (bookRead.innerHTML === "Did not read it yet") {
+    bookRead.innerHTML = "Read it";
+  } else {
+    bookRead.innerHTML = "Did not read it yet";
+  }
+}
+
 function updateBooks(book, author, pages, read) {
   let newBook = new Book(book, author, pages, read);
   addBookToLibrary(newBook);
+  createHTML(book, author, pages, read);
 }
 
 function checkData() {
@@ -74,12 +88,16 @@ function checkData() {
 function hideForm() {
   if (document.getElementById("form1").style.display === "block") {
     document.getElementById("form1").style.display = "none";
+    document.getElementById("newBook").disabled = false;
+    document.getElementById("form1").classList.remove("flip-scale-2-hor-top");
   }
 }
 
 // eslint-disable-next-line no-unused-vars
 function openForm() {
   document.getElementById("form1").style.display = "block";
+  document.getElementById("newBook").disabled = true;
+  document.getElementById("form1").classList.add("flip-scale-2-hor-top");
 }
 
 function buttonChecked() {
